38f1b495d63f8749934048ccdd7df6a774d8cf13
unknown <es-jukim@smilegate.com>
2018-06-07 15:18:52 +0900

    input addTodo 까지

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# TodoHeader 구현
-# css 대충 알아서 구현하면 됨
-# rem 기준 폰트의 상대적 크기. 궁금하면 구글링(keyword: css rem)
+# TodoInput component 구현
+# v-model vuejs api 참조 data 양방향 바인딩
+# vue-devtools 정도는 크롬에서 검색해서 설치하고 써보자
+# chrome Vue 화면 보면 data 바이딩을 확인할수 있다.
+# addTodo method make 에서 쓰는 this vue 객체이다.(모르면 시간 지나면 알아짐.. 객체의 속성 값을 가져오는구나 정도로 넘어가자)
+# 서버가 없으니 로컬스토리지를 사용한다.
+# localStorage.setItem 모르면 검색하자.
+# 크롬 개발자도구 f12 application tab 확인해보면 키 값이 추가됨을 알수 있다.
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        input
+        <input type="text" v-model="newTodoItem">
+        <button @click="addTodo">add</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            newTodoItem: ''
+        };
+    },
+    methods: {
+        addTodo() {
+            console.log('clicked', this.newTodoItem);
+            localStorage.setItem(this.newTodoItem, this.newTodoItem)
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
