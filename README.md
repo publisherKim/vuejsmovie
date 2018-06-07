ce3e1a00f06c9e52a7ac29635f532f2c27b3dabf
unknown <es-jukim@smilegate.com>
2018-06-07 17:25:39 +0900

    footer 구현

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# todolist complete 만들기
-# checkbox
-# toggleComplete method 만들기
-# todoInput에 completed 속성 추가하기
-# JSON.stringify(obj) 문자열로 변경하기 locaStorage 특성상 javascript object를 저장하면 타입이 저장된당.[object object]
-# localStorage 저장 방법이 바꼈기 때문에 꺼내올때 JSON.parse 해줘야함, 문자열을 다시 객체로
-# toggleCompleted logic this.todoItems[index].completed = !this.todoItems[index].completed || todoItem.completed = !todoItem.completed
-# localSotrage 삭제후 저장
-# localStorage.removeItem(todoItem.item);
-# localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+# todoFooter 구현
+# clearTodo method 구현
+# localStorage.clear() api 호출
--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        footer
+    <div class="clearAllContainer">
+        <span
+            class="clearAllBtn"
+            @click="clearTodo"
+        >
+            Clear All
+        </span>
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    methods: {
+        clearTodo() {
+            localStorage.clear();
+        }
     }
+}
 </script>
 
 <style scoped>
-
+    .clearAllContainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        font-weight: bold;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn {
+        color: #e20303;
+        display: block;
+    }    
 </style>
